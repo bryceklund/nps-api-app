b0e5e8fb1c43b184dd4a743645ba1f4973938df9
Bryce Eklund <bryce.eklund@gmail.com>
2019-05-16 17:33:36 -0700

    began input-parsing in js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const apiKey = 'Sm4Y8pZ3YnZNN5CjYwzPKo0d2M8pJsomPaeA3gIG';
+const searchURL = 'developer.nps.gov/api/v1/parks?statecode=';
+
+
+function formatInput(states, max) {
+    const statesArr = states.replace(/\s/g, '').split(",")
+
+}
+
+function watchForm() {
+    $('form').submit(function(event) {
+        const states = $('#states-input').val();
+        const resultsMax = $('#resultsMax-input').val();
+        $('form').trigger('reset');
+
+    });
+}
+
+$(watchForm());
